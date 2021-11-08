bfa1d04d41e6f47b1916c2f080486842f0635fb9
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-11-09 01:01:31 +0530

    send

--- src/components/PartnerCompnay/index.js
+++ src/components/PartnerCompnay/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {AboutContainer,CardListRow} from './PartnerCompnayElements'
-import KalT from '../../images/kalTire.png';
-import KalTL from '../../images/KalTrailerandLeasing.png';
-import KalF from '../../images/KalFreight.png';
+import KalTire from '../../images/kalTire.png';
+import KalTrailerAndLeasing from '../../images/KalTrailerandLeasing.png';
+import KalFreight from '../../images/KalFreight.png';
 import { animateScroll as scroll,Link } from 'react-scroll';
 
 const PartnerCompnay = ({isOpen,toggle}) => {
@@ -11,9 +11,9 @@ const PartnerCompnay = ({isOpen,toggle}) => {
          
         
          <CardListRow>
-         <a href="https://www.kaltires.com/"><img src={KalT} alt="Kaltire website link" /></a>
-         <a href="https://kaltrailers.com/" ><img src={KalTL} alt="kaltrailers website link" /></a>
-         <a  href="https://kalpartz.com/" ><img src={KalF} alt="kalparts website link" /></a>
+         <a href="https://www.kaltires.com/"><img src={KalTire} alt="Kaltire website link" /></a>
+         <a href="https://kaltrailers.com/" ><img src={KalTrailerAndLeasing} alt="kaltrailers website link" /></a>
+         <a  href="https://kalpartz.com/" ><img src={KalFreight} alt="kalparts website link" /></a>
          
          
              
